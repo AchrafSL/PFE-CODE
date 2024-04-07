4788ac57f466bd9c2e6e704775741645c042449e
AchrafSL <97637989+AchrafSL@users.noreply.github.com>
2024-04-07 15:14:37 +0000

    Update1

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template,redirect
+
+app = Flask(__name__)
+
+#Home
+@app.route('/home', methods=["GET"])
+@app.route('/', methods=["GET"])
+def home():
+    return render_template("Home.html")
+
+# Offers
+@app.route('/Offers', methods=["GET"])
+def Offers():
+    return render_template("Offers.html")
+
+# ContactUS
+@app.route('/ContactUS', methods=["GET"])
+def ContactUS():
+    return render_template("ContactUS.html")
+
+# FeedBacks
+@app.route('/FeedBacks', methods=["GET"])
+def FeedBacks():
+    return render_template("FeedBacks.html")
+
+# AboutUS
+@app.route('/AboutUs', methods=["GET"])
+def AboutUS():
+    return render_template("AboutUs.html")
+
+# Cart
+@app.route('/Cart', methods=["GET"])
+def Cart():
+    return render_template("Cart.html")
+
+# Login
+@app.route('/Login', methods=["GET"])
+def Login():
+    return render_template("Login.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
