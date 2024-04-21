6c126b917a0b9a176d746cec5e48bec319817709
AchrafSL <97637989+AchrafSL@users.noreply.github.com>
2024-04-21 20:26:16 +0100

    Delete-Confirmation | UPDATE

--- static/JS/USER_Page.js
+++ static/JS/USER_Page.js
@@ -6,6 +6,25 @@ function redirectDelete(variable2)
     form.submit();
 }
 
+function redirectLogout(variable2){
+    var form = document.forms[variable2];
+    form.action = "/logout";
+    form.method = "POST";
+    form.submit();
+}
+
+
 function handleFileUpload(){
     document.getElementById('uploadForm').submit();
+}
+
+function confirmDelete()
+{
+    var result = confirm("Are you sure you want to delete your account?");
+    if(result){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
